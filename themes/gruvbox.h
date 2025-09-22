f2d075aa6df9fbacff7d1aae4534ced971b433d6
Your Name <you@example.com>
2025-09-22 22:49:00 +0530

    added shiftview,themes,config.h

---
+++ themes/gruvbox.h
@@ -0,0 +1,10 @@
+// background
+static const char col_gray1[]       = "#282828";
+static const char col_gray2[]       = "#32302f";
+static const char col_gray3[]       = "#ebdbb2";
+static const char col_gray4[]       = "#1e1e2e";
+// windowname
+static const char col_cyan[]        = "#458588";
+static const char col_cyan1[]        = "#689d6a";
+static const char border_col[]      = "#cc241d";
+static const char normborder_col[]  = "#a89984";
