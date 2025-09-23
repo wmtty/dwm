395cad0f0c243d585aa36d6b5cb7e8feeec8dfc8
Your Name <you@example.com>
2025-09-23 13:54:40 +0530

    bar height

--- config.h
+++ config.h
@@ -27,6 +27,7 @@ static const int vertpad            = 2;       /* vertical padding of bar */
 static const int sidepad            = 2;       /* horizontal padding of bar */
 static const int showbar = 1;     /* 0 means no bar */
 static const int topbar = 1;      /* 0 means bottom bar */
+static const int user_bh            = 25;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[] = {
     /* "JetBrainsMono Nerd Font Propo:size=9:style=Medium"}; */
     "ComicShannsMono Nerd Font Propo:size=10:style=Medium"};
